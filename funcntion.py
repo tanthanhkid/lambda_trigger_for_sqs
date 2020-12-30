be407c1ff10aa04c7b8ff461586cdcd6dfacf8f4
tanthanhkid <tanthanhkid@gmail.com>
2020-12-30 22:45:33 +0700

    fix code and add txt file

--- funcntion.py
+++ funcntion.py
@@ -22,16 +22,16 @@ def lambda_handler(event, context):
        
         # Replace sender@example.com with your "From" address.
         # This address must be verified with Amazon SES.
-        SENDER = "tan thanh <trantanthanh95@live.com>"
+        SENDER = "tan thanh <email của người gửi>"
         
         # Replace recipient@example.com with a "To" address. If your account 
         # is still in the sandbox, this address must be verified.
-        RECIPIENT = "tanthanhkid@gmail.com"
+        RECIPIENT = "email của người nhận"
         
         # Specify a configuration set. If you do not want to use a configuration
         # set, comment the following variable, and the 
         # ConfigurationSetName=CONFIGURATION_SET argument below.
-        CONFIGURATION_SET = "noti-service-configure-set"
+        #CONFIGURATION_SET = "noti-service-configure-set"
         
         # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
         AWS_REGION = "ap-southeast-1"
